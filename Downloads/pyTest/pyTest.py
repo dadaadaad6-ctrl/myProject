d7e42a0f348994db91a638b10b5d14e64c0fc7b8
dadaadaad6-ctrl <dadaadaad6@gmail.com>
2026-03-11 03:40:14 -0700

    First AI

---
+++ Downloads/pyTest/pyTest.py
@@ -0,0 +1,17 @@
+print("Hello, I am an AI bot, what is your name?")
+name = input()
+
+print(f"Nice to meet you, {name}")
+
+print("How are you feeling?")
+feeling = input()
+
+if feeling=="Good":
+    print("That's nice to hear!")
+elif feeling=="Bad":
+    print("That's sad to hear")
+else:
+    print("I see. Sometimes it's hard to put feelings into words")
+
+print(f"It was nice meeting you, {name}. Goodbye!")
+
